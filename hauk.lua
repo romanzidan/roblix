6ef3cc8c91f08fcd46cfdca728dcca2c35af6f1f
Roman Zidan <romanzidanramadhan@gmail.com>
2025-08-17 08:44:51 +0800

    new

--- hauk.lua
+++ hauk.lua
@@ -9,7 +9,7 @@ local plr = Players.LocalPlayer
 --[[
 	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
 ]]
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SomebodyThatIsGreen/yodo/refs/heads/main/asddads"))()
+
 
 
 -- === Character helper ===

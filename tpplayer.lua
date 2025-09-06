dc25a538816f3c087f9a5676190f0068ba3ffaab
Roman Zidan <romanzidanramadhan@gmail.com>
2025-09-06 20:13:44 +0800

    final

--- tpplayer.lua
+++ tpplayer.lua
@@ -315,7 +315,7 @@ TpTrollBtn.MouseButton1Click:Connect(function()
         startWalkFling()
         task.spawn(function()
             while TpTrollActive do
-                task.wait(0.11)
+                task.wait(0.1)
                 if CurrentTarget and CurrentTarget.Character and CurrentTarget.Character:FindFirstChild("HumanoidRootPart") then
                     local hrp = getRootPart()
                     if hrp then

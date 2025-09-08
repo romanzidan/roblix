2a8c8db34fbae1b53ff89935a3ec4cbb0a81f9b1
Roman Zidan <romanzidanramadhan@gmail.com>
2025-09-08 08:44:06 +0800

    new

--- tpplayer.lua
+++ tpplayer.lua
@@ -315,25 +315,41 @@ TpTrollBtn.MouseButton1Click:Connect(function()
     end
 end)
 
+-- Flag biar tidak double call
+local isCancelling = false
+
 -- === Fungsi Cancel Spectate ===
 local function CancelSpectate()
+    if isCancelling then return end -- cegah pemanggilan ulang
+    isCancelling = true
+
+    TpTrollBtn.BackgroundColor3 = Color3.fromRGB(200, 100, 0)
+    TpTrollBtn.Text = "TP TROLL"
+    stopWalkFling()
+    task.wait(0.4)
+
     if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("Humanoid") then
         Camera.CameraSubject = LocalPlayer.Character.Humanoid
         if LastPosition and LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+            LocalPlayer.Character.HumanoidRootPart.CFrame = LastPosition
+            task.wait(0.5)
             LocalPlayer.Character.HumanoidRootPart.CFrame = LastPosition
             LastPosition = nil
         end
         CurrentTarget = nil
         StatusLbl.Text = "Not Spectating"
     end
+
     -- paksa off TP Troll + stop fly/walkfling
     TpTrollActive = false
-    TpTrollBtn.BackgroundColor3 = Color3.fromRGB(200, 100, 0)
-    TpTrollBtn.Text = "TP TROLL"
-    task.wait(1)
+    task.wait(1.3)
+
     stopFly()
-    stopWalkFling()
+
+    -- reset flag supaya bisa dipanggil lagi
+    isCancelling = false
 end
+
 
 
 --// Logic
@@ -365,7 +381,7 @@ local function UpdatePlayerList()
 
                         -- tunggu sampai HumanoidRootPart muncul dan benar-benar ter-replicate
                         local hrp
-                        for i = 1, 20 do -- coba 100x (≈5 detik dengan wait 0.05)
+                        for i = 1, 10 do -- coba 100x (≈5 detik dengan wait 0.05)
                             hrp = char:FindFirstChild("HumanoidRootPart")
                             if hrp then break end
                             task.wait(0.05)
@@ -377,11 +393,7 @@ local function UpdatePlayerList()
                             StatusLbl.Text = "Spectating " .. plr.Name
                         else
                             -- gagal load (misalnya player keluar atau streaming gagal)
-                            StarterGui:SetCore("SendNotification", {
-                                Title = "Spectate gagal",
-                                Text = plr.Name .. " belum bisa dilihat.",
-                                Duration = 5
-                            })
+
                             if CurrentTarget == plr then
                                 CancelSpectate()
                             end
